0b98ada81387f19da844f4d1801c2aca86fc7930
Edson José dos Santos <edsonj82@outlook.com>
2019-11-12 17:30:33 -0200

    add a new todo to the list

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,5 +1,11 @@
 ///<reference types="cypress"/>
 
-it('should navigate to the TodoMVC App', () => {
-    cy.visit('todomvc-app-for-testing.surge.sh');
-});
+it('should be able to add a new todo to the list', () => {
+    cy.visit('todomvc-app-for-testing.surge.sh')
+
+    cy.get('.new-todo').type('Clean room{enter}')
+
+    cy.get('.toggle').click()
+
+    cy.contains('Clear completed').click()
+})
