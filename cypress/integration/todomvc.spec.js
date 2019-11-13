76de425535b3c48f16f30d25220e79a5e9d5aefa
Edson José dos Santos <edsonj82@outlook.com>
2019-11-13 21:55:28 -0200

    Validations all tasks per functions

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,18 +1,26 @@
 ///<reference types="cypress"/>
 
 it('should be able to add a new todo to the list', () => {
+
+})
+
+it('should add a new todo to the list', () => {
     cy.visit('todomvc-app-for-testing.surge.sh')
 
     cy.get('.new-todo').type('Clean room{enter}')
 
     cy.get('label').should('have.text','Clean room')
     cy.get('.toggle').should('not.be.checked')
+})
 
+it('should mark a todo as completed', () => {
     cy.get('.toggle').click()
     cy.get('label').should('have.css','text-decoration-line','line-through')
+})
 
+it('should clear completed todos', () => {
     cy.contains('Clear').click()
 
     cy.get('.todo-list').should('not.have.descendants','li')
     // cy.contains('Clear completed').click()
-})
+});
