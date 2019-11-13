4cc4c0684703266eb8d5ac72dfdad502486b0df8
Edson José dos Santos <edsonj82@outlook.com>
2019-11-13 21:43:38 -0200

    Create validations in TODO List

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -5,7 +5,14 @@ it('should be able to add a new todo to the list', () => {
 
     cy.get('.new-todo').type('Clean room{enter}')
 
-    cy.get('.toggle').click()
+    cy.get('label').should('have.text','Clean room')
+    cy.get('.toggle').should('not.be.checked')
 
-    cy.contains('Clear completed').click()
+    cy.get('.toggle').click()
+    cy.get('label').should('have.css','text-decoration-line','line-through')
+
+    cy.contains('Clear').click()
+
+    cy.get('.todo-list').should('not.have.descendants','li')
+    // cy.contains('Clear completed').click()
 })
