5dd64994cd82c3bd9d34ed8262402326291adea8
Edson José dos Santos <edsonj82@outlook.com>
2019-11-13 22:03:29 -0200

    Testing just a specific function (it.only)

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,7 +1,7 @@
 ///<reference types="cypress"/>
 
 describe('todo actions',() =>{
-    it('should add a new todo to the list', () => {
+    it.only('should add a new todo to the list', () => {
         cy.visit('todomvc-app-for-testing.surge.sh')
     
         cy.get('.new-todo').type('Clean room{enter}')
