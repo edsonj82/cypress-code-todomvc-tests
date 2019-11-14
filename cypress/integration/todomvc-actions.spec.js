8a2bc4a716fd568c3effbe5ef85bce014aea733f
Edson José dos Santos <edsonj82@outlook.com>
2019-11-13 22:22:18 -0200

    Created todomvc-filtering and rename file todomvc-actions

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -1,16 +1,17 @@
 ///<reference types="cypress"/>
 
 describe('todo actions',() =>{
-    it.only('should add a new todo to the list', () => {
+    beforeEach(()=>{
         cy.visit('todomvc-app-for-testing.surge.sh')
-    
         cy.get('.new-todo').type('Clean room{enter}')
     
+    })
+    it('should add a new todo to the list', () => {
         cy.get('label').should('have.text','Clean room')
         cy.get('.toggle').should('not.be.checked')
     })
     
-    it('should mark a todo as completed', () => {
+    it.only('should mark a todo as completed', () => {
         cy.get('.toggle').click()
         cy.get('label').should('have.css','text-decoration-line','line-through')
     })
